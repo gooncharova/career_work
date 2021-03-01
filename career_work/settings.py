071978755f443fdb591ea9b492c98b087e5d0365
Ольга <oe.goncharova@gmail.com>
2021-03-01 07:25:13 +0300

    update settings

--- career_work/settings.py
+++ career_work/settings.py
@@ -47,7 +47,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
